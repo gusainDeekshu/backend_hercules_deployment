1fbd645380d79e9c8a5601dfeeb58701dfe9b154
gusainDeekshu <gusaindeekshant@gmail.com>
2026-01-01 14:59:12 +0530

    cors added

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   // Use specific origins to allow credentials (cookies/headers) securely
-  origin: ['http://localhost:3000', 'http://localhost:3001'], 
+  origin: ['http://localhost:3000', 'http://localhost:3001','https://admin-hercules-deployment.vercel.app','https://user-hercules-deployment.vercel.app'], 
   credentials: true
 }));
 app.use(express.json());
